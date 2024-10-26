2179bbe859dc922ad082f7e841b006993b8782cf
126-Arvind <arvindyadav2021126@gmail.com>
2024-10-26 16:42:06 +0530

    Tiktok clone app

---
+++ layouts/MainLayout.vue
@@ -0,0 +1,18 @@
+<template>
+   
+    <TopNav/>
+    <div 
+    :class="route.fullPath === '/' ? 'max-w-[1150px]' : ''"
+    class="flex justify-between mx-auto w-full lg:px-2.5 px-0">
+    <div>
+        <SideNavMain />
+    </div>
+    
+</div>
+
+    </template>
+    
+    <script setup>
+   const route = useRoute();
+    
+    </script>
